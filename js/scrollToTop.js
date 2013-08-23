71199aa516f35e52bcbca6d937947ac59bf2c25e
Andriy Zhdanov <azhdanov@gmail.com>
2013-08-23 16:45:33 +0200

    scroll to nav

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,7 +1,7 @@
 var scrollToTop = function(element) {
 	var el = (typeof element == 'string') ? $('#' + element) : $($('a.back-to-top').attr("data-href"));
 	$("html, body").animate({
-		scrollTop: el.offset().top + "px"
+		scrollTop: el.offset().top - 45 + "px"
 	}, {
 		duration: 400,
 		easing: "swing"
