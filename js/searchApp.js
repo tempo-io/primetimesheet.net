596feee4e7d9294d9367efa1230d1cdaf7b8a6fa
Andriy Zhdanov <azhdanov@gmail.com>
2017-07-23 16:24:21 +0200

    automatic commit by punch-git-publisher

--- js/searchApp.js
+++ js/searchApp.js
@@ -5,7 +5,7 @@ angular.module('searchApp',[]).filter('matchesQuery', function(){
         }
         var alternate = query.replace(/ /g,"_").toLowerCase();
         var lcQuery = query.toLowerCase();
-        var arrayToReturn = [];        
+        var arrayToReturn = [];
         for (var i=0; i<items.length; i++){
             if ((items[i].title && items[i].title.toLowerCase().indexOf(lcQuery) !== -1)
                            || items[i].text.indexOf(alternate) !== -1) {
@@ -19,7 +19,7 @@ angular.module('searchApp',[]).filter('matchesQuery', function(){
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 }).controller('SearchWikiCtrl', function($scope, $http) {
   $scope.query = "";
-  $scope.posts = [];
+  $scope.posts = 'test';
   $http.get('/contents.json').success(function(data) {
     $scope.contents = data.contents;
   });
--- js/site.js
+++ js/site.js
@@ -1,80 +1,20 @@
-var scrollToTop = function(event) {
-	var el = $(event.target.attributes['href']);
-	$("html, body").animate({
-		scrollTop: el.offset().top + "px"
-	}, {
-		duration: 400,
-		easing: "swing"
-	});
-	return false;
-}
-
 $(function() {
-	$('a.back-to-top').live('click', scrollToTop);
+  $('#dropdownMenu').on('show.bs.dropdown', function () {
+    //https://stackoverflow.com/questions/32381746/handle-twitter-bootstrap-dropdown-clipped-issue-having-overflow-auto-on-containe
+    var $this = $(this);
+    var $dropdown = $this.find('.dropdown-menu');
+    $dropdown.css('position', 'fixed');
+    $dropdown.css('top', /* fixed $this.offset().top + */ $this.height() + 20 /* padding */);
+  });
 
-    function el(event) {
-	    return event.target.localName == 'span' ?
-	        $(event.target) : $('span', event.target);
-    }
+  var el = function (event) {
+    return event.target.localName == 'span' ?
+      $(event.target) : $('span', event.target);
+  };
 
-	$('h3').live('mouseleave', function(event) {
-		el(event).hide();
-	}).live('mouseenter', function(event) {
-		el(event).show();
-	})
-
-    $('#slider').nivoSlider();
-
-    $('#at4-soc').click(function() {
-        $.getScript('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5206946308988819', function() {
-            addthis.layers({
-              'theme' : 'transparent',
-              'share' : {
-                'position' : 'left',
-                'numPreferredServices' : 5
-              }   
-            });
-        });
-    });
-
-    var loading = function(isLoading) {
-      var $e = $('#loading');
-      isLoading === true ? $e.show() : $e.hide();
-    }
-
-    var submitted = false;
-    $('#demoLicense form').validate({
-        submitHandler: function(form, event) {
-            if (submitted) {
-                return;
-            }
-            submitted = true;
-            loading(true);
-            var $form = $(form);
-            $.ajax({
-                url: form.action,
-                cache: false,
-                type: 'POST',
-                data: $form.serialize(),
-                dataType: "jsonp",
-                //timeout: 1000,
-                crossDomain: true,
-                success: function (data, status) {
-                    if (data == 'OK') {
-                        $form.parent().css('color', '#317eac').html("You should recieve license to specified email shortly.")
-                    } else {
-                        $form.parent().css('color', '#d14').html("Error: " + data);
-                    }
-                },
-                error: function (xOptions, textStatus) {
-                    submitted = false;
-                    console.log(textStatus);
-                },
-                complete: function() {
-                    loading(false);
-                }
-            });
-        }
-     });
+  $('h3').on('mouseleave', function(event) {
+    el(event).hide();
+  }).on('mouseenter', function(event) {
+    el(event).show();
+  });  
 });
-
